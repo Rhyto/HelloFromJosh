36d9f2ee45bec7b9d2f2d58d9c6fefa322a461cb
Nate M <64175378+nmccoard@users.noreply.github.com>
2020-05-14 20:48:27 -0400

    Update Main.java

--- src/phillips/Main.java
+++ src/phillips/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Josh.");
-	System.out.println("Hello, Josh. My name is Nate. It is very nice to meet you.")
+	System.out.println("Hello, Josh. My name is Nate. It is very nice to meet you.");
     }
 }
